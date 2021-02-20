23219d3478293d74f3905dd0882d11fcfc52ed5a
ChaTho <cgds0855@hotmail.com>
2021-02-20 14:42:24 +0300

    Test

---
+++ Entities/Concrete/Tissue.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    class Tissue:IEntity
+    {
+        public int Id { get; set; }
+        public int TypeId { get; set; }
+        public int RegionId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ Entities/Concrete/Type.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    class Type:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Origin { get; set; }
+    }
+}
