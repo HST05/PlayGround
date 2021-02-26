98c210bc0014852efd14dbcbd99d3636c845bf2f
ChaTho <cgds0855@hotmail.com>
2021-02-26 11:35:08 +0300

    ...

--- WebAPI/Controllers/TissueImagesController.cs
+++ WebAPI/Controllers/TissueImagesController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Entities.Concrete;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.Files;
 
 namespace WebAPI.Controllers
 {
@@ -14,22 +17,53 @@ namespace WebAPI.Controllers
     public class TissueImagesController : ControllerBase
     {
         private ITissueImageService _tissueImageService;
-
-        public TissueImagesController(ITissueImageService tissueImageService)
+        public static IWebHostEnvironment _webHostEnvironment;
+        public TissueImagesController(ITissueImageService tissueImageService, IWebHostEnvironment webHostEnvironment)
         {
             _tissueImageService = tissueImageService;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [HttpPost("imageadd")]
-        public IActionResult Add(TissueImage tissueImage)
+        public string Add([FromForm] Image image)
         {
-            var result = _tissueImageService.Add(tissueImage);
-            if (result.Success)
+
+            try
             {
-                return Ok(result);
+                if (image.ImageFile.Length > 0)
+                {
+                    string path = "C:\\Users\\Windows 8\\Desktop\\ChaTho The Programmer\\test\\web api\\";
+                    Guid guid = Guid.NewGuid();
+                    string ext = System.IO.Path.GetExtension(image.ImageFile.FileName);
+                    string imageName = $"{guid}" + ext;
+
+                    using (FileStream fileStream = System.IO.File.Create(path + imageName))
+                    {
+                        image.ImageFile.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "uploaded.";
+                    }
+                }
+                else
+                {
+                    return "not uploaded";
+                }
+            }
+            catch (Exception e)
+            {
+
+                return e.Message;
             }
 
-            return BadRequest(result);
+
+
+            //var result = _tissueImageService.Add(tissueImage);
+            //if (result.Success)
+            //{
+            //    return Ok(result);
+            //}
+
+            //return BadRequest(result);
         }
     }
 }
